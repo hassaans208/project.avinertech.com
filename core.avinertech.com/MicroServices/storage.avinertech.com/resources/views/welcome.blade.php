28189e99a99f5d5fc7e48b851104f7d151e18d1c
hassaanshariq <amazonhm97@gmail.com>
2025-02-03 22:20:13 +0500

    Storage

--- core.avinertech.com/MicroServices/storage.avinertech.com/resources/views/welcome.blade.php
+++ core.avinertech.com/MicroServices/storage.avinertech.com/resources/views/welcome.blade.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Share Files with your friends in a safe ecosystem! AvinerTech">
 
-        <title>Laravel</title>
+        <title>AvinerTech</title>
 
         <!-- Fonts -->
+{{--        <link rel="favicon">--}}
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
