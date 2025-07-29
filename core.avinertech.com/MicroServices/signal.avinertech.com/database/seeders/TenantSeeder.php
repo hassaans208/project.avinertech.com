5ae80c5eef3f91adcf0b883619e42f5e0b0a7826
Hassaan avinertech <hassaan@kali>
2025-07-29 18:46:49 +0500

    Signals Added

--- core.avinertech.com/MicroServices/signal.avinertech.com/database/seeders/TenantSeeder.php
+++ core.avinertech.com/MicroServices/signal.avinertech.com/database/seeders/TenantSeeder.php
@@ -15,24 +15,28 @@ class TenantSeeder extends Seeder
     {
         $tenants = [
             [
+                'id' => 1,
                 'name' => 'Example.com Tenant',
                 'host' => 'example.avinertech.com',
                 'status' => 'active',
                 'package' => 'free_package',
             ],
             [
+                'id' => 2,
                 'name' => 'Test Site Tenant',
                 'host' => 'prototype.avinertech.com',
                 'status' => 'active',
                 'package' => 'basic_package',
             ],
             [
+                'id' => 3,
                 'name' => 'Demo Application',
                 'host' => 'demo.avinertech.com',
                 'status' => 'active',
                 'package' => 'professional_package',
             ],
             [
+                'id' => 4,
                 'name' => 'Blocked Tenant',
                 'host' => 'blocked.avinertech.com',
                 'status' => 'blocked',
