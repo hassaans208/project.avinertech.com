31eaae64c72dec09c47f1aca99053c49f4cca8b9
Hassaan avinertech <hassaan@kali>
2025-08-02 07:02:33 +0500

    Fix Signal

--- core.avinertech.com/MicroServices/universities.avinertech.com/vite.config.js
+++ core.avinertech.com/MicroServices/universities.avinertech.com/vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    // solve cors error
+    server: {
+        cors: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
+        },
+        allowedHosts: ['localhost', '127.0.0.1', 'qs.avinertech.com'],
+        
+    },
 });
