8be2f020652a313ea637a3a17c2aca2bafdd4ee8
Hassaan avinertech <hassaan@kali>
2025-08-02 07:11:48 +0500

    Fix Signal

--- core.avinertech.com/MicroServices/universities.avinertech.com/vite.config.js
+++ core.avinertech.com/MicroServices/universities.avinertech.com/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
             'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization',
         },
-        allowedHosts: ['localhost', '127.0.0.1', 'qs.avinertech.com'],
+        allowedHosts: 'all',
         
     },
 });
